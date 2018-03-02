a0056bf56bbb06cfcad1b4009521d3aea229d6e9
NicolasHernandez13144 <36998221+NicolasHernandez13144@users.noreply.github.com>
2018-03-02 13:00:18 -0500

    Crear metodo imprimir

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("");
+		imprimir("Hola amigos");
 	}
-
+public static void imprimir (String str){
+	System.out.println(str);
+}
 }
